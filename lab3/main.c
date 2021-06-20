1bf7bdb24f1289c9010cb1fb964987a3377306b3
Pigeongela <82166857+Pigeongela@users.noreply.github.com>
2021-06-20 19:18:46 +0300

    Add files via upload

---
+++ lab3/main.c
@@ -0,0 +1,10 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+#include "bmp.h"
+#define WIN32_MEAN_AND_LEAN
+
+int main() {
+	read("file_1.bmp", "file_2.bmp", "file_out.bmp");
+	return 0;
+}
